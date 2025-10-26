17747f02570723a0cdcff9928cb3ff6184ec64d5
thomasbrh <thomas.bruch@student.heaj.be>
2025-10-26 02:18:41 +0200

    Modif mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projets/TouchOfSushi/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [
     vue(),
     vueDevTools(),
